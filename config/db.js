944a365d813a839d517a2dbb5f6e57a9162e1148
syncure <richvargast@gmail.com>
2021-03-27 20:02:39 -0500

    Version 1 Backend

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+require("dotenv").config({ path: "variables.env" });
+
+// process.env.DB_MONGO
+
+const conectarBD = async () => {
+  try {
+    await mongoose.connect(process.env.DB_MONGO, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    });
+    console.log('DB CONECTADA');
+  } catch (error) {
+    console.log(error);
+    process.exit(1); // Detener la app
+  }
+};
+
+module.exports = conectarBD;
